2112e99d70c8707967f03b7819eb20061684af7a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-30 11:12:56 +0300

    init: fix-string-case

---
+++ tasks/fix-string-case.js
@@ -0,0 +1,20 @@
+function solve(s){
+  const length = s.length;
+  let j = 0;
+
+  for (let i = 0; i < length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      j += 1;
+    }
+  }
+
+  return j <= length - j ? s.toLowerCase() : s.toUpperCase();
+}
+
+
+
+console.log(solve('code'),'code');
+console.log(solve('CODe'),'CODE');
+console.log(solve('COde'),'code');
+console.log(solve('Code'),'code');
+  
