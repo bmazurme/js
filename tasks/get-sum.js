40ae005f4b5f6bdf8c50054d4eb65cf30e856d27
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-10-21 11:11:33 +0300

    init: get-sum

---
+++ tasks/get-sum.js
@@ -0,0 +1,21 @@
+function getSum(a, b) {
+  if (a === b) {
+    return a;
+  }
+ 
+  let sum = 0;
+  let left = a < b ? a : b;
+  let right = a > b ? a : b;
+ 
+  for(let i = left; i <= right; i++) {
+    sum += i;
+  }
+ 
+  return sum;
+}
+ 
+console.log(getSum(0,-1), -1);
+console.log(getSum(0, 1),  1);
+console.log(getSum(2, 2),  2);
+ 
+console.log(getSum(0, 1),  1);
