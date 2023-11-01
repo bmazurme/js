c9f01bb24ab51f0d5d609d16b710526a0690a290
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-11-01 08:55:32 +0300

    fix: valid-braces

--- tasks/valid-braces.js
+++ tasks/valid-braces.js
@@ -36,3 +36,38 @@ console.log(validBraces(')(}{][', false));
 console.log(validBraces('())({}}{()][][', false));
 console.log(validBraces('(((({{', false));
 console.log(validBraces('}}]]))}])', false));
+
+// function validBraces(braces) {
+//   const set = { ')': '(', ']': '[', '}': '{' };
+//   const keys = Object.values(set);
+//   const path = [];
+ 
+//   for(let i = 0; i < braces.length; i++) {
+//     if (keys.includes(braces[i])) {
+//       path.push(braces[i]);
+//     } else {
+//       if (path[path.length - 1] === set[braces[i]]) {
+//         path.pop();
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+ 
+//   return path.length === 0;
+// }
+ 
+// console.log(validBraces('()'), true);
+// console.log(validBraces('[]'), true);
+// console.log(validBraces('{}'), true);
+// console.log(validBraces('(){}[]'), true);
+// console.log(validBraces('([{}])'), true);
+// console.log(validBraces('(}'), false);
+// console.log(validBraces('[(])'), false);
+// console.log(validBraces('({})[({})]'), true);
+// console.log(validBraces('(})'), false);
+// console.log(validBraces('(({{[[]]}}))'), true);
+// console.log(validBraces('{}({})[]'), true);
+// console.log(validBraces(')(}{]['), false);
+// console.log(validBraces('())({}}{()][]['), false);
+// console.log(validBraces('(((({{'), false);
