b4ccbcccfaf4ff9ae49903fe9397a30d2ca3d83a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-11-20 08:06:02 +0300

    init: get-number-from-string

---
+++ tasks/get-number-from-string.js
@@ -0,0 +1,8 @@
+function getNumberFromString(s) {
+  return Number(s.replace(/\D/g, ''));
+}
+// return +s.replace(/\D/g, "");
+ 
+console.log(getNumberFromString('1'), 1);
+console.log(getNumberFromString('123'), 123);
+console.log(getNumberFromString('this is number: 7'), 7);
