1405b415cd4d0ee006ff7dfa0fdd476c16600bf0
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-12-07 08:31:26 +0300

    init: bus-stops

---
+++ tasks/bus-stops.js
@@ -0,0 +1,10 @@
+const number = function(busStops) {
+  const { i, o } = busStops.reduce((a, x) => ({ i: a.i += x[0], o: a.o += x[1] }), { i: 0, o: 0 });
+  
+  return i - o;
+}
+ 
+console.log(number([[10,0],[3,5],[5,8]]),5);
+console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]),17);
+console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]),21);
+console.log(number([[0,0]]),0);
