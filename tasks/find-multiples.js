719c99fdd877129e536520bf5adfe8e251cc97e5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-01-03 13:39:52 +0300

    init: find-multiples

--- tasks/find-multiples.js
+++ tasks/find-multiples.js
@@ -12,4 +12,4 @@ console.log(findMultiples(5, 25), [5, 10, 15, 20, 25]);
 console.log(findMultiples(1, 2), [1, 2]);
 console.log(findMultiples(5, 7), [5]);
 console.log(findMultiples(4, 27), [4, 8, 12, 16, 20, 24]);
-console.log(findMultiples(11, 54), [11, 22, 33, 44]);
+console.log(findMultiples(11, 54), [11, 22, 33, 44]);multipleOfIndex
