d1d27f3fbef84bc487716207d29a5d9201d01969
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-01-09 20:07:21 +0300

    fix: stray

--- tasks/stray.js
+++ tasks/stray.js
@@ -3,14 +3,14 @@ function stray(numbers) {
   // return numbers.filter((x) => x === arr[0]).length === 1 ? arr[0] : arr[1];
   return numbers.reduce((a, b) => a ^ b);
  
-  // for (var i in numbers) {
+  // for (let i in numbers) {
   //   if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
   //     return numbers[i];
   //   }
   // }
 }
  
-// console.log(stray([1, 1, 2]), 2);
-// console.log(stray([1, 2, 1]), 2);
-// console.log(stray([2, 1, 1]), 2);
-// console.log(stray([17, 17, 3, 17, 17, 17, 17]), 3);
+console.log(stray([1, 1, 2]), 2);
+console.log(stray([1, 2, 1]), 2);
+console.log(stray([2, 1, 1]), 2);
+console.log(stray([17, 17, 3, 17, 17, 17, 17]), 3);
