1120291970057a1d0bc64c2e7356ec492a7803f3
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-01-23 14:35:36 +0300

    init: whatday

---
+++ tasks/whatday.js
@@ -0,0 +1,19 @@
+function whatday(num) { 
+  const days = {
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday',
+  };
+  
+  return days[num] ?? 'Wrong, please enter a number between 1 and 7';
+}
+ 
+console.log(whatday(1), 'Sunday');
+console.log(whatday(2), 'Monday');
+console.log(whatday(3), 'Tuesday');
+console.log(whatday(8), 'Wrong, please enter a number between 1 and 7');
+console.log(whatday(20), 'Wrong, please enter a number between 1 and 7');
