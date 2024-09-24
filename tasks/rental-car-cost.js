b2a4c8c3183a754835dc95fc5effa915695e3049
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-09-24 08:11:54 +0300

    init: rental-car-cost

---
+++ tasks/rental-car-cost.js
@@ -0,0 +1,17 @@
+function rentalCarCost(d) {
+  const total = 40 * d;
+  const off = d >= 7 ? 50 : d >= 3 ? 20 : 0;
+
+  return total - off;
+}
+
+console.log(rentalCarCost(1), 40);
+console.log(rentalCarCost(2), 80);
+console.log(rentalCarCost(3), 100);
+console.log(rentalCarCost(4), 140);
+console.log(rentalCarCost(5), 180);
+console.log(rentalCarCost(6), 220);
+console.log(rentalCarCost(7), 230);
+console.log(rentalCarCost(8), 270);
+console.log(rentalCarCost(9), 310);
+console.log(rentalCarCost(10), 350);  
