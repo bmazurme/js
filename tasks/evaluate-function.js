8ebf24d73f9eac492d726a9dc0603d70dd7e4c64
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-10-09 20:24:18 +0300

    init: evaluate-function

---
+++ tasks/evaluate-function.js
@@ -0,0 +1,9 @@
+function evaluateFunction(a, x, b, c) {
+  const y = a * x * x + b * x + c;
+  return y;
+}
+ 
+console.log(evaluateFunction(-8, -5, -2, 7), -183);
+console.log(evaluateFunction(8, 2, 9, -10), 40);
+// console.log(evaluateFunction(8, 2, 9, -10), 40);
+// -8 -5 -2 7
