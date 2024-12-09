50c07dd3af5bf23138ab2d240d02b3b4541da34c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-12-09 22:01:19 +0300

    init: s7a

---
+++ tasks/sprint7/a/a.js
@@ -0,0 +1,44 @@
+const _readline = require('readline');
+const fileStream = require('fs').createReadStream('input.txt');
+const _reader = _readline.createInterface({ input: fileStream });
+const _inputLines = [];
+_reader.on('line', line => _inputLines.push(line));
+fileStream.on('end', solve);
+ 
+class InitData {
+  constructor() {
+    this._curLine = 0;
+    this.rows = 0;
+    this.array = [];
+  }
+
+  readLine() {
+    const line = _inputLines[this._curLine];
+    this._curLine++;
+
+    return line;
+  }
+ 
+  readData() {
+    this.rows = Number(this.readLine());
+    this.array = this.readLine().split(' ').map(Number);
+  }
+}
+
+function getMax(arr) {
+  let result = 0;
+
+  for  (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) {
+      result += arr[i] - arr[i - 1];
+    }
+  }
+
+  return result;
+}
+
+function solve() {
+  const initData = new InitData();
+  initData.readData();
+  console.log(getMax(initData.array));
+}
