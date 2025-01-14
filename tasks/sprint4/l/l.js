3d0c7532e6a356cd2a1fa27efb95f4db226f7226
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-01-14 19:10:56 +0300

    fix: s4l

--- tasks/sprint4/l/l.js
+++ tasks/sprint4/l/l.js
@@ -1,56 +1,96 @@
 const _readline = require('readline');
-const fileStream = require('fs').createReadStream('input.txt');
+const fs = require('fs');
 
-const _reader = _readline.createInterface({
-  // input: process.stdin
-  input: fileStream
-});
+class InitData {
+  constructor(stream = fs.createReadStream('input.txt')) {
+    this._curLine = 0;
+    this.n = 0;
+    this.k = 0;
+    this.line = null;
+    this.stream = stream;
+    this._readline = _readline
+      .createInterface({ input: stream })
+      .on('line', (line) => this.set(line));
+  }
 
-const _inputLines = [];
-let _curLine = 0;
+  set(line) {
+    if (this._curLine === 0) {
+      const [n, k] = line.split(' ').map(Number);
+      this.n = n;
+      this.k = k;
+    } else {
+      this.line = line;
+    }
 
-_reader.on('line', line => {
-  _inputLines.push(line);
-});
+    this._curLine++;
+  }
+}
 
-// process.stdin.on('end', solve);
-fileStream.on('end', solve);
+function prefixHash(a, m, s) {
+  const acc = [s[0].charCodeAt() % m];
+
+  for (let i = 1; i < s.length; i++) {
+    acc[i] = ((acc[i - 1] * a % m) + s[i].charCodeAt()) % m;
+  }
+
+  return acc;
+}
+
+function modFix(n, m) {
+  return ((n % m) + m) % m;
+}
+
+function powFix(a, pow, m) {
+  let result = 1;
+  let base = a % m;
+
+  while (pow > 0) {
+    if (pow % 2 === 1) {
+      result = (result * base) % m;
+    }
+    base = (base * base) % m;
+    pow = Math.floor(pow / 2);
+  }
+
+  return result;
+}
+
+function getKey(left, right, acc, m, x) {
+  return modFix(acc[right - 1] - modFix(((acc[left - 2] | 0) * x), m), m);
+}
 
 function getSubstr(line, n, k) {
-  const dict = {};
-  for (let i = 0; i < line.length; i++) {
-    for (let j = i + 1; j < line.length + 1; j++) {
-      const key = line.substring(i, j);
-      if (dict[key] === undefined) {
-        dict[key] = { lenght: j - i, count: 1, value: key, index: i };
-      } else {
-        dict[key].count += 1;
-      }
+  const a = 31;
+  const a1 = 37;
+  const m = 10000003;
+  const m1 = 10000009;
+  const dict = new Map();
+  const result = [];
+  const acc = prefixHash(a, m, line);
+  const acc1 = prefixHash(a1, m1, line);
+  const x = powFix(a, n, m);
+  const x1 = powFix(a1, n, m1);
+
+  for (let left = 0; left <= line.length - n; left++) {
+    let right = left + n - 1;
+    const key = `${getKey(left, right, acc, m, x)}${getKey(left, right, acc1, m1, x1)}`;
+
+    if (dict.has(key)) {
+      dict.get(key).count++;
+    } else {
+      dict.set(key, { count: 1, index: left - 1 });
     }
   }
-  return Object.values(dict).filter((x) => x.lenght === n && x.count >= k).map((x) => x.index);
+
+  for (const [key, value] of dict) {
+    if (value.count >= k) {
+      result.push(value.index);
+    }
+  }
+
+  return result;
 }
 
-function solve() {
-  const [n, k] = readInt();
-  const line = readLine();
-
-  console.time();
-  const result = getSubstr(line, n, k);
-  console.timeEnd();
-  console.log(result.join(' '));
-}
-
-function readInt() {
-  const n = _inputLines[_curLine].trim(' ').split(' ').map(Number);
-  _curLine++;
-
-  return n;
-}
-
-function readLine() {
-  const str = _inputLines[_curLine].trim(' ');
-  _curLine++;
-
-  return str;
-}
+const initData = new InitData();
+const solve = () => console.log(getSubstr(initData.line, initData.n, initData.k).join(' '));
+initData.stream.on('end', solve);
